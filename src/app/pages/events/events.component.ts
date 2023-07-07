277856652287eeadaa495e7fa83dec59d857c685
VERNIERELoic <loic.verniere@cpe.fr>
2023-07-07 20:49:17 +0200

    pagination admin

--- src/app/pages/events/events.component.ts
+++ src/app/pages/events/events.component.ts
@@ -31,6 +31,8 @@ export class EventsComponent {
   tabs: any = [];
   selectedIndex = 0;
   today: any;
+  page = 1;
+  pageSize = 10;
 
   constructor(private nzMessageService: NzMessageService,
     private fb: UntypedFormBuilder,
